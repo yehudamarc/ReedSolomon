1a03217cb0ca254621005e97a4a9002e159f0fe3
yehudamarc <63788386+yehudamarc@users.noreply.github.com>
2020-07-14 14:36:09 +0300

    Update decoder.py
    transform vectors to bivariant polynomials

--- decoder.py
+++ decoder.py
@@ -18,3 +18,18 @@ for row in range(n):
     m[row] = [(a_array[row]^i)*(y_array[row]^j) for i in range(deg_a +1) for j in range(deg_y+1)]
 
 kernel_vectors = m.right_kernel_matrix()
+
+number_of_rows = len(kernel_vectors.rows())
+
+bivariant_polinomyals_array = [x+y for i in range(number_of_rows)]
+
+for num in range(number_of_rows):
+    vector = kernel_vectors[num]
+    R.<x,y> = F[]
+    f = 0
+    index_counter = 0
+    for i in range(deg_a+1):
+        for j in range(deg_y+1):
+            f += vector[index_counter]*(x^i)*(y^j)
+            index_counter += 1
+    bivariant_polinomyals_array[num] = f
