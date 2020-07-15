836dc09e87603689e5ac442af1225aecc434381c
yehudamarc <63788386+yehudamarc@users.noreply.github.com>
2020-07-15 14:35:41 +0300

    Update decoder.py
    added factorization

--- decoder.py
+++ decoder.py
@@ -5,7 +5,7 @@ k = 4
 n = 4
 
 a_array = [0,1,2,3]
-y_array = [24, 10, 28, 32]
+y_array = [10, 24, 10, 28]
 
 deg_a = sqrt(k*n)
 deg_y = sqrt(n/k)
@@ -21,11 +21,13 @@ kernel_vectors = m.right_kernel_matrix()
 
 number_of_rows = len(kernel_vectors.rows())
 
+R.<x,y> = F[]
+T.<x> = F[]
+
 bivariant_polinomyals_array = [x+y for i in range(number_of_rows)]
 
 for num in range(number_of_rows):
     vector = kernel_vectors[num]
-    R.<x,y> = F[]
     f = 0
     index_counter = 0
     for i in range(deg_a+1):
@@ -33,3 +35,14 @@ for num in range(number_of_rows):
             f += vector[index_counter]*(x^i)*(y^j)
             index_counter += 1
     bivariant_polinomyals_array[num] = f
+
+filtered_pols = []
+
+for i in range(number_of_rows):
+    g = factor(bivariant_polinomyals_array[i])
+    roots = [pol[0] for pol in list(g)]
+    for pol in roots:
+        if (y-pol in T):
+            filtered_pols.append([y-pol])
+
+filtered_pols
